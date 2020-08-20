911d609e5858534cf615a249a8fd67d9ba111951
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-20 20:04:57 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,10 @@
 status = "rescued"
-def rescue_princess_peach(status)
+
+def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
 
-rescue_princess_peach
-puts status
+#rescue_princess_peach
+#puts status
